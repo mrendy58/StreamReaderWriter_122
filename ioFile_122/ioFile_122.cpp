f42cc7509bca38b6e904789ab7ecadccb1129568
mrendy58 <derendyyy58@gmail.com>
2023-06-19 11:34:41 +0700

    membuat ofstream untuk membuaka file dengan mode menulis

--- ioFile_122/ioFile_122.cpp
+++ ioFile_122/ioFile_122.cpp
@@ -9,7 +9,7 @@ int main() {
     // membuka file dalam mode menulis.
     ofstream outfile;
     // menunjuk ke sebuah nama file
-    outfile.open("contohfile.txt");
+    outfile.open("contohfile.txt"); 
 
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
