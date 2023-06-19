b87e229df85e154c121342dafe3b4bad6dedbc91
mrendy58 <derendyyy58@gmail.com>
2023-06-19 11:35:30 +0700

    membuat loop menulis dengan menggunakan while true

--- ioFile_122/ioFile_122.cpp
+++ ioFile_122/ioFile_122.cpp
@@ -18,7 +18,7 @@ int main() {
         cout << "- ";
         // mendapatkan setiap karakter dalam satu baris
         getline(cin, baris);
-        // loop akan berhenti jika anda memasukan karakter q
+        // loop akan berhenti jika anda memasukan karakter q 
         if (baris == "q") break;
         // Menulis dan memasukan nilai dari 'baris' ke dalam file
         outfile << baris << endl;
