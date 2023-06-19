d0a17b3773bfdd2d3f84d29b92ae0c927474ca28
mrendy58 <derendyyy58@gmail.com>
2023-06-19 11:49:42 +0700

    membuat perulangan menggunakan while

--- ioFile_122/ioFile_122.cpp
+++ ioFile_122/ioFile_122.cpp
@@ -42,7 +42,7 @@ int main() {
             cout << baris << '\n';
         }
         // tutup file tersebut setelah selesai
-        infile.close();
+        infile.close(); 
     }
     // jika tidak ditemukan file maka akan menampilkan ini
     else cout << "Unable to open file";
